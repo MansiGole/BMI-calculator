da892f8b5fdf54d344fd276d78f39ee0f741dd8b
MansiGole <155070403+MansiGole@users.noreply.github.com>
2025-04-12 14:25:01 +0530

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -25,10 +25,10 @@ import java.util.Scanner;
             }
     
             // 🚶‍♂️User Input
-            System.out.print("\nEnter your weight in kilograms (e.g., 68.5): ");
+            System.out.print("\nEnter your weight in kilograms: ");
             double weight = scanner.nextDouble();
     
-            System.out.print("Enter your height in meters (e.g., 1.75): ");
+            System.out.print("Enter your height in meters: ");
             double height = scanner.nextDouble();
     
             // 🧮 BMI Calculation
